559d201475e5ee3edfb0aa2f7b6e5badd153678c
roberto ricci <roberto.rricci@gmail.com>
2023-08-26 00:47:16 -0300

    ajustes

--- app_home.py
+++ app_home.py
@@ -10,7 +10,7 @@ def home():
     #col2.image('./imagen/analisequant_logo-removebg.png')
 
     st.markdown("<h2 style='text-align: center; color: rgb(74, 113, 152);'> Aplicativo de inteligência virtual </h2>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center; color: rgb(74, 113, 152)'>Bot ChatGPT Desenvolvido durante Treinamento A Era Do Cientista de Dados</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center; color: rgb(74, 113, 152)'>Bot ChatGPT Desenvolvido durante Treinamento A Era Do Ciêntista de Dados</h4>", unsafe_allow_html=True)
     st.markdown("<h5 style='text-align: center; color: rgb(74, 113, 152)'></h5>", unsafe_allow_html=True)
     
     st.markdown("<h4 style='text-align: center; color: rgb(74, 113, 152)'><p> Quer saber mais ?  click no link  <a style='color:white;text-align:center;' href='https://cientistadedadosnapratica.com.br/aula1' target='_blank'>A Era dos Dados</a> </p></h4>", unsafe_allow_html=True)
